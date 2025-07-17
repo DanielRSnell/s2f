409b8954587a884e52f223d03d3ce399c2c85dad
Daniel Snell <broke@MacBook-Pro.local>
2025-07-17 13:25:43 -0400

    deployment fixes

--- .netlify/build/chunks/astro_DekQ1YKY.mjs
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server_CtSX6tUJ.mjs';
-import 'clsx';
--- .netlify/build/chunks/content-modules_DWl9LaQ3.mjs
+++
@@ -1,4 +0,0 @@
-const contentModules = new Map([
-["src/content/pages/elements.mdx", () => import('./elements_qplAXaup.mjs')]]);
-
-export { contentModules as default };
